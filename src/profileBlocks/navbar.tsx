166f07d9f14aed131aa4c13ea37e5113b6093a88
Sriharsha K <sriharshak@192.168.1.4>
2024-09-13 23:37:20 +0530

    navbar token

--- src/profileBlocks/navbar.tsx
+++ src/profileBlocks/navbar.tsx
@@ -1,36 +1,35 @@
-import React, {useEffect, useState, useContext, useMemo} from 'react';
-import {AuthenticationContext} from '../context';
+import React, { useEffect, useState, useContext, useMemo } from 'react';
+import { AuthenticationContext } from '../context';
 import Button from '../components/button';
 import isTokenExpired from '../utils/commonUtils';
 
 const Navbar = () => {
 
-    const {profile, setProfile} = useContext(AuthenticationContext);
+    const { profile, setProfile } = useContext(AuthenticationContext);
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/profile`, { credentials: 'include' })
-          .then(response => {
-            if (response.ok) return response.json();
-            throw new Error('Authentication failed');
-          })
-          .then(data => {
-            console.log(data.token);
-            // Store token in localStorage or context
-            if(localStorage.getItem('token')!==null) {
-                localStorage.setItem('token', data.token);
-            }
+            .then(response => {
+                if (response.ok) return response.json();
+                throw new Error('Authentication failed');
+            })
+            .then(data => {
+                // Store token in localStorage or context
+                if (localStorage.getItem('token') === null) {
+                    localStorage.setItem('token', data.token);
+                }
 
-            setProfile(data.user)
-          })
-          .catch(error => console.error(error));
-      }, []);
-    
+                setProfile(data.user)
+            })
+            .catch(error => console.error(error));
+    }, []);
+
 
     const handleLogin = async () => {
         try {
             // const response = await axios.get('http://localhost:5000/login');
             // const authUrl = response.data.url;
-            window.location.href = process.env.REACT_APP_API_URL+"/auth/google";
+            window.location.href = process.env.REACT_APP_API_URL + "/auth/google";
 
         } catch (error) {
             console.error('Error initiating Twitter login', error);
@@ -39,7 +38,7 @@ const Navbar = () => {
     };
 
     const handleLogOut = () => {
-        window.location.href = process.env.REACT_APP_API_URL+"/logout";
+        window.location.href = process.env.REACT_APP_API_URL + "/logout";
     }
 
     return (
