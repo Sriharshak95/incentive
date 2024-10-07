98501b90182d9a715e3ab40a3048abf8a76185e2
Sriharsha K <sriharshak@192.168.1.2>
2024-10-07 19:32:17 +0530

    mobile responsive window

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { AppLayout } from './layout';
-import SidebarLayout from './SidebarLayout';
 export const App = () => (
   // <div className="container mx-auto h-screen">
     // <SidebarLayout />
